60505be54514d8e9033b25cf732ed03818201999
Maria Kalusz <mkalusz1@gmail.com>
2019-04-24 13:12:24 -0700

    adding movie class

---
+++ project/app/src/main/java/com/example/myapp/MovieData.java
@@ -0,0 +1,20 @@
+package com.example.ad340project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+
+public class MovieData extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_data);
+
+
+
+    }
+}
