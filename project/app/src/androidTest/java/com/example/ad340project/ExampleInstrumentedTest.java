ea6e918d8c2f3d7ebcd90482f9a321601f521402
Maria Kalusz <mkalusz1@gmail.com>
2019-04-24 13:15:07 -0700

    adding hw3

--- project/app/src/androidTest/java/com/example/ad340project/ExampleInstrumentedTest.java
+++ project/app/src/androidTest/java/com/example/ad340project/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.myapp;
+package com.example.ad340project;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.myapp", appContext.getPackageName());
+        assertEquals("com.example.ad340project", appContext.getPackageName());
     }
 }
--- project/app/src/test/java/com/example/ad340project/ExampleUnitTest.java
+++ project/app/src/test/java/com/example/ad340project/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.myapp;
+package com.example.ad340project;
 
 import org.junit.Test;
 
