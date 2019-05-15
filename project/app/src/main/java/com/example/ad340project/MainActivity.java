fc2dc1f8538206e9de01d8656f2926b5127e547c
siergieyk <siergieyk@gmail.com>
2019-05-14 19:51:00 -0700

    Updated Assignment4

--- project/app/src/main/java/com/example/ad340project/MainActivity.java
+++ project/app/src/main/java/com/example/ad340project/MainActivity.java
@@ -18,6 +18,8 @@ import android.app.Activity;
 import android.view.Menu;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.view.MenuInflater;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         startActivity(strIntent);
     }
 
-    public class Listener implements  android.view.View.OnClickListener {
+    public class Listener implements android.view.View.OnClickListener {
 
         CharSequence c;
 
@@ -54,7 +56,97 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onClick(View v) {
-            Toast.makeText(getApplicationContext(), c,Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), c, Toast.LENGTH_SHORT).show();
         }
     }
+
+    private Toolbar mTopToolbar;
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater menuInflater = getMenuInflater();
+//        menuInflater.inflate(R.menu.menu, menu);
+//        return true;
+//
+//    }
+
+        @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_favorite:
+                Intent intent = new Intent(MainActivity.this, aboutPage.class);
+                this.startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.action_favorite:
+//                Intent intent = new Intent(MainActivity.this, aboutPage.class);
+//                startActivity(intent);
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//
+//        }
+//    }
 }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_favorite) {
+//            Intent strIntent = new Intent(this, aboutPage.class);
+//            startActivity(strIntent);
+////            Toast.makeText(MainActivity.this, "About", Toast.LENGTH_LONG).show();
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//
+//    }}
+
+
+//        switch(item.getItemId()) {
+//            case R.id.action_favorite:
+//                Intent intent = new Intent(this, aboutPage.class);
+//                startActivity(intent);
+//                return true;
+//
+//        }} }
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch(item.getItemId()) {
+//            case R.id.action_favorite:
+//                Intent intent = new Intent(MainActivity.this, aboutPage.class);
+//                startActivity(intent);
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//
+//        }
+//    }
+
+
+
--- project/app/src/main/java/com/example/ad340project/MovieList.java
+++ project/app/src/main/java/com/example/ad340project/MovieList.java
@@ -3,6 +3,7 @@ package com.example.ad340project;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -44,8 +45,10 @@ public class MovieList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_list);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar2);
         setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
 
         HashMap<String, String> mMap = new HashMap<>();
         List<String> listZ = new ArrayList<>();
